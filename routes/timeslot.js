048d319a60b0ae0582b77181e545a703c690dd5f
Richard Novenius <novenius@student.chalmers.se>
2022-01-09 22:07:43 +0100

    improved QoS method

--- routes/timeslot.js
+++ routes/timeslot.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('tcp://127.0.0.1:1883') 
+var client = mqtt.connect('tcp://127.0.0.1:1883')
 var express = require('express');
 var router = express.Router();
 var maxTries = 10
@@ -11,87 +11,86 @@ var i = 0;
 var q = 0;
 var times
 
-router.get('/timeslots/:id', async function(req, res, next) {
+router.get('/timeslots/:id', async function (req, res, next) {
     // let result = await bookTimeHelper(req.params.id)
     let result = await getAll()
     console.log("resultat: ")
     console.log(result)
     res.json(result)
-  });
+});
 
-  router.get('/timeslots/', async function(req, res, next) {
+router.get('/timeslots/', async function (req, res, next) {
     let result = await getAll()
     res.json(result)
-  });
+});
 
-client.subscribe('/timeslots/book/response')
 
-async function bookTimeHelper(id){
+
+async function bookTimeHelper(clinicId, timeslotId) {
     tryCount = 0
-    currentId = id
     client.subscribe('/timeslots/book/response')
-    for(i=0;i <= maxTries;i++){
+    for (i = 0; i <= maxTries; i++) {
         await timer(100)
-        bookTime(id)
-        console.log("xaxaxaxa")
+        bookTime(clinicId, timeslotId)
+
     }
-    console.log("after")
-    if (!messageRecieved){
-        return '404'
-    }else{
+    if (!messageRecieved) {
+        console.log("Message not recieved, unsubscribing")
+        return "Message not recieved"
+    } else {
         messageRecieved = false
-        return '200'
+        console.log("Message Recieved")
+        return "Message recieved"
     }
 }
 
 
-async function bookTime(clinicId, timeslotId){
+async function bookTime(clinicId, timeslotId) {
     let message = `${clinicId}` + " " + `${timeslotId}`
-    console.log(message)
-   client.publish('/timeslots/book/',message)
+    client.publish('/timeslots/book/', message)
+    return message
 }
-client.on('connect', function(){
+client.on('connect', function () {
     console.log('Client has subscribed successfully')
 })
 client.on('message', async function (topic, message) {
     let statusCode = message.toString()
-    if (statusCode == '200'){
+    if (statusCode == '200') {
         messageRecieved = true
         i = maxTries
         console.log(statusCode)
         client.unsubscribe('/timeslots/book/')
-        
+
     }
-    else if (statusCode == '404' || message == '403'){
+    else if (statusCode == '404' || statusCode == '403') {
         messageRecieved = true
         i = maxTries
         console.log(statusCode)
         client.unsubscribe('/timeslots/book/')
     }
-    else { 
-            client.unsubscribe('/timeslots/book/')
-        }
-    if(topic == '/timeslots/all/'){
-        console.log("meddelande")
+    else {
+        client.unsubscribe('/timeslots/book/')
+    }
+    if (topic == '/timeslots/all/') {
         const timeslots = await parseJson(message)
         times = timeslots
         q = maxTries
     }
 })
-async function parseJson(message){
+async function parseJson(message) {
     console.log('parse')
     return JSON.parse(message.toString())
 }
 
-async function getAll(){
+async function getAll() {
     client.subscribe('/timeslots/all/')
     timeslots = ''
-    for(q=0;q <= maxTries;q++){
+    for (q = 0; q <= maxTries; q++) {
         await timer(100)
-        client.publish('/timeslots/request/all/',"penis vagene unid")
+        client.publish('/timeslots/request/all/', "fetch")
     }
     return times
 }
-bookTime("61d8203942783868569c9382","61d8203942783868569c9388")
+bookTimeHelper("61d8203942783868569c9382", "61d8203942783868569c9393")
 
 module.exports = router;
